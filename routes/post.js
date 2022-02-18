83ccd00a60ba7a489eb35e98a1a6b0254dc4a87e
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-02-19 00:44:41 +0900

    ADD: 좋아요 / 취소 api 추가 FIX: isLiking도 반환하도록 수정

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const sequelize = require("sequelize");
 
 const router = express.Router();
 
-const { Post, User } = require("../models");
+const { Post, User, Like } = require("../models");
 
 // 포스트 작성 POST /api/post
 router.post("/", async(req, res) => {
@@ -24,15 +25,28 @@ router.post("/", async(req, res) => {
     });
 
     const newPost = await Post.findOne({
-      where: post.id,
-      attributes: { exclude: ["updatedAt"] },
+      where: { id: post.id },
+      attributes: [
+        "id", "title", "thumbnail", "context", "createdAt",
+        [sequelize.literal("(SELECT COUNT(*) FROM Likes where Likes.postId=post.id)"), "likersCount"],
+      ],
       include: {
         model: User,
         attributes: ["id", "nickname"],
       },
     });
 
-    return res.status(201).json(newPost);
+    const isLiking = await Like.findOne({
+      where: {
+        postId: newPost.id,
+        UserId: sampleUserId,
+      },
+    });
+
+    return res.status(201).send({
+      post: newPost,
+      isLiking: isLiking ? true : false,
+    });
   } catch(error) {
     console.error(error);
     return res.status(500).json({ msg: "서버 내부 에러" });
@@ -56,14 +70,36 @@ router.patch("/:postId", async (req, res) => {
     });
     if(!prevPost) { return res.status(400).json({ msg: "해당 포스트를 찾을 수 없습니다." }); };
 
-    const newPost = await prevPost.update({
+    await prevPost.update({
       title,
       thumbnail: thumbnail ? thumbnail : "",
       preview,
       context,
     });
 
-    return res.status(201).json(newPost);
+    const newPost = await Post.findOne({
+      where: { id: prevPost.id },
+      attributes: [
+        "id", "title", "thumbnail", "context", "createdAt",
+        [sequelize.literal("(SELECT COUNT(*) FROM Likes where Likes.postId=postId)"), "likersCount"],
+      ],
+      include: {
+        model: User,
+        attributes: ["id", "nickname"],
+      },
+    });
+
+    const isLiking = await Like.findOne({
+      where: {
+        postId: newPost.id,
+        UserId: sampleUserId,
+      },
+    });
+
+    return res.status(201).send({
+      post: newPost,
+      isLiking: isLiking ? true : false,
+    });
   } catch(error) {
     console.error(error);
     return res.status(400).json({ msg: "서버 내부 에러" });
@@ -81,6 +117,69 @@ router.delete("/:postId", async (req, res) => {
     if(!isDeleted) { return res.status(400).json({ msg: "해당 포스트를 찾을 수 없습니다." }) };
 
     return res.status(200).json({ msg: "포스트를 삭제하였습니다." });
+  } catch(error) {
+    console.error(error);
+    return res.status(400).json({ msg: "서버 내부 에러" });
+  };
+});
+
+// 게시글 좋아요 GET /api/post/3/likes
+router.get("/:postId/likes", async (req, res) => {
+  try {
+    const { postId } = req.params;
+    
+    const sampleUserId = 1;
+
+    const [like, isLiked] = await Like.findOrCreate({
+      where: { postId: postId },
+      defaults: {
+        UserId: sampleUserId,
+      }
+    });
+    if(!isLiked) { return res.status(400).send({ msg: "이미 좋아요를 한 포스트입니다." }); };
+
+    const post = await Post.findOne({
+      where: { id: postId },
+      attributes: [
+        [sequelize.literal("(SELECT COUNT(*) FROM Likes where Likes.postId=postId)"), "likersCount"],
+      ],
+    });
+
+    return res.status(201).send({
+      post: post,
+      isLiking: true,
+    });
+  } catch(error) {
+    console.error(error);
+    return res.status(400).json({ msg: "서버 내부 에러" });
+  };
+});
+
+// 게시글 좋아요 취소 DELETE /api/post/3/likes
+router.delete("/:postId/likes", async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    const sampleUserId = 1;
+
+    await Like.destroy({
+      where: {
+        postId: postId,
+        userId: sampleUserId,
+      },
+    });
+
+    const post = await Post.findOne({
+      where: { id: postId },
+      attributes: [
+        [sequelize.literal("(SELECT COUNT(*) FROM Likes where Likes.postId=postId)"), "likersCount"],
+      ],
+    });
+
+    return res.status(201).send({
+      post: post,
+      isLiking: false,
+    })
   } catch(error) {
     console.error(error);
     return res.status(400).json({ msg: "서버 내부 에러" });
