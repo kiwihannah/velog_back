b8ed23f8f416ebe5faf69c7e764137f501383209
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-02-19 19:27:08 +0900

    FIX: DB subcomment 관계 제거

--- models/comment.js
+++ models/comment.js
@@ -25,7 +25,6 @@ module.exports = (sequelize, DataTypes) => {
   Comment.associate = (db) => {
     db.Comment.belongsTo(db.User); // 하나의 코멘트에는 한 명의 작성자만 있다.
     db.Comment.belongsTo(db.Post); // 하나의 코멘트는 하나의 포스트에는 존재할 수 있다.
-    db.Comment.hasMany(db.SubComment); // 댓글에는 여러 개의 대댓글이 있을 수 있다.
   };
 
   return Comment;
