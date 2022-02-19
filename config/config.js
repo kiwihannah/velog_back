5e5b437cbdcc505be2124c11d1c5d406e026f73c
김승호 <naho199345@naver.com>
2022-02-19 10:33:14 +0900

    user

--- config/config.js
+++ config/config.js
@@ -3,24 +3,24 @@ dotenv.config();
 
 module.exports = {
   "development": {
-    "username": "root",
+    "username": "mem_sh",
     "password": process.env.DB_PASSWORD,
     "database": "velog_development",
-    "host": "127.0.0.1",
+    "host": "3.35.169.150",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
+    "username": "mem_sh",
     "password": process.env.DB_PASSWORD,
     "database": "velog_test",
-    "host": "127.0.0.1",
+    "host": "3.35.169.150",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
+    "username": "mem_sh",
     "password": process.env.DB_PASSWORD,
     "database": "velog_production",
-    "host": "127.0.0.1",
+    "host": "3.35.169.150",
     "dialect": "mysql"
   }
 }
