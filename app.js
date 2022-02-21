c2e3884bae6ae67ec606d710873b77e0433e9b91
Hannah Jung <83942678+kiwihannah@users.noreply.github.com>
2022-02-21 16:45:28 +0900

    Update app.js
    port 설정 변경

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use("/api", userRouter);
 app.use("/api/post/:postId/comment", commentRouter);
 app.use("/api/image", imageRouter);
 
-app.listen(8023, () => {
+app.listen(3000, () => {
   console.log('서버가 켜졌어요!');
 });
 
