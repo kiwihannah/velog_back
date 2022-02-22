1dccd614a4c630763a216e5ccd2afcb6a18b98d5
김승호 <naho199345@naver.com>
2022-02-22 15:06:37 +0900

    회원탈퇴, 비밀번호 수정 기능 구현

--- routes/users.js
+++ routes/users.js
@@ -15,5 +15,10 @@ router.post('/user/login', UserController.UserLogIn.post);
 // 사용자인증 미들웨어
 router.get('/user/me', authMiddlleware, UserController.UserConfirm.get);
 
+// 회원삭제
+router.delete('/user/delUser', authMiddlleware, UserController.DeleteUser.delete);
+
+// 회원수정
+router.patch('/user/editUser', authMiddlleware, UserController.Usermodification.patch);
 
 module.exports = router;
